896453fd939619fbf1c8c60417d18affa437c2de
Victor Andrade de Oliveira <exceltic-services.victor.andrade@hp.com>
2019-12-06 02:15:29 +0100

    Storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/html';
+import '!style-loader!css-loader!sass-loader!../src/styles/main.scss';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
