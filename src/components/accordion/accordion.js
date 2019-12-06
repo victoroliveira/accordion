6c71a9b76236617dea7be084c177c668ad6c0869
Victor Andrade de Oliveira <exceltic-services.victor.andrade@hp.com>
2019-12-06 13:08:29 +0100

    Added event on click

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -3,10 +3,31 @@ customElements.define(
   class Accordion extends HTMLDListElement {
     constructor() {
       super();
+
+      this.dts = [...this.querySelectorAll('dt')];
+      this.dds = [...this.querySelectorAll('dd')];
     }
 
     connectedCallback() {
+      this._styling();
+      this.dts.map(el => el.addEventListener('click', this._toggle.bind(this)));
+    }
+
+    _styling() {
       this.classList.add('accordion');
+
+      this.dts.map(item => {
+        item.classList.add('accordion__title');
+      });
+
+      this.dds.map(item => {
+        item.classList.add('accordion__content');
+      });
+    }
+
+    _toggle(evt) {
+      const contentEl = evt.target.nextElementSibling;
+      contentEl.classList.add('accordion__content--open');
     }
   },
   { extends: 'dl' }
