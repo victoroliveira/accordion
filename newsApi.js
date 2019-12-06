236f31e1579a852988e75b9b1a7d0565ae6ddca7
Victor Andrade de Oliveira <exceltic-services.victor.andrade@hp.com>
2019-12-07 00:09:34 +0100

    Last item as dynamic

---
+++ newsApi.js
@@ -0,0 +1,4 @@
+const apiKey = '75ec205a13864bcc9ab23c488f0ce093';
+const topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+export { apiKey, topHeadlinesUrl };
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,4 +1,23 @@
 import '../src/components/accordion/accordion';
+import { topHeadlinesUrl } from '../newsApi.js';
+
+const fetchNews = async () => {
+  const res = await fetch(topHeadlinesUrl);
+  const json = await res.json();
+
+  const accordion = document.querySelector('dl[is="my-accordion"]');
+  const elTitle = accordion.querySelector('dt:last-of-type');
+  const elContent = accordion.querySelector('dd:last-of-type');
+
+  json.articles.map(article => {
+    elTitle.innerHTML = article.title;
+    elContent.innerHTML = `<p>${article.content}</p>`;
+  });
+};
+
+window.addEventListener('load', () => {
+  fetchNews();
+});
 
 export default {
   title: 'Demo',
@@ -10,5 +29,7 @@ export const accordion = () => `
     <dd><p>My content</p></dd>
     <dt>My Title 2</dt>
     <dd><p>My content 2</p></dd>
+    <dt>My Title 3</dt>
+    <dd><p>My content 3</p></dd>
   </dl>
 `;
