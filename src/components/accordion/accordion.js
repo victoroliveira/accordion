25a7d01696a908c2c856b289601d92e41521ce0a
Victor Andrade de Oliveira <victoroliveira.eng@gmail.com>
2019-12-09 02:39:45 +0100

    Export accordion class

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,47 +1,45 @@
-customElements.define(
-  'my-accordion',
-  class Accordion extends HTMLDListElement {
-    constructor() {
-      super();
+export default class Accordion extends HTMLDListElement {
+  constructor() {
+    super();
 
-      this.dts = [...this.querySelectorAll('dt')];
-      this.dds = [...this.querySelectorAll('dd')];
-    }
+    this.dts = [...this.querySelectorAll('dt')];
+    this.dds = [...this.querySelectorAll('dd')];
+  }
 
-    connectedCallback() {
-      this._styling();
-      this.dts.map(el => el.addEventListener('click', this._toggle.bind(this)));
-    }
+  connectedCallback() {
+    this._styling();
+    this.dts.map(el => el.addEventListener('click', this._toggle.bind(this)));
+  }
 
-    _styling() {
-      this.classList.add('accordion');
+  _styling() {
+    this.classList.add('accordion');
 
-      this.dts.map(item => {
-        item.classList.add('accordion__title');
-      });
+    this.dts.map(item => {
+      item.classList.add('accordion__title');
+    });
 
-      this.dds.map(item => {
-        item.classList.add('accordion__content');
-      });
-    }
+    this.dds.map(item => {
+      item.classList.add('accordion__content');
+    });
+  }
 
-    // In case to select other item, search for opened items
-    _collapseAll(current) {
-      this.dds.map(el => {
-        if (current !== el) {
-          el.removeAttribute('selected');
-          el.classList.remove('accordion__content--open');
-        }
-      });
-    }
+  // In case to select other item, search for opened items
+  _collapseAll(current) {
+    this.dds.map(el => {
+      if (current !== el) {
+        el.removeAttribute('selected');
+        el.classList.remove('accordion__content--open');
+      }
+    });
+  }
 
-    _toggle(evt) {
-      const contentEl = evt.target.nextElementSibling;
+  _toggle(evt) {
+    const contentEl = evt.target.nextElementSibling;
 
-      this._collapseAll(contentEl);
-      contentEl.classList.add('accordion__content--open');
-      contentEl.setAttribute('selected', '');
-    }
-  },
-  { extends: 'dl' }
-);
+    this._collapseAll(contentEl);
+    contentEl.classList.add('accordion__content--open');
+    contentEl.setAttribute('selected', '');
+  }
+}
+
+customElements.define('my-accordion', Accordion, { extends: 'dl' });
